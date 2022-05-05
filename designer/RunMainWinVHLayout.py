0c1a71d1510f1ff765f32001bd8fe0c1e5e51406
Edshine <accolade2001@126.com>
2022-05-05 15:26:44 +0800

    second commit

---
+++ designer/RunMainWinVHLayout.py
@@ -0,0 +1,17 @@
+import sys
+import MainWinHVLayout
+from PyQt5.QtWidgets import QApplication,QMainWindow
+
+if __name__ == '__main__':
+    #创建一个QApplication类的实例
+    app = QApplication(sys.argv)
+    #创建一个QMainWindow类的对象
+    mainwindow = QMainWindow()
+    #引用MainWinHorizontalLayout类并创建它的实例
+    ui = MainWinHVLayout.Ui_MainWindow()
+    #创建该窗口的组件
+    ui.setupUi(mainwindow)
+    #展示该窗口
+    mainwindow.show()
+    #退出该程序
+    sys.exit(app.exec_())
